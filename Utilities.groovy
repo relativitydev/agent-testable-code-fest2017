0cb3db2db2e6421fe05f0015b467e510d4191a8d
Priya Pande <ppande@kcura.com>
2018-02-20 15:46:14 -0600

    Added a change to utilities file

--- Utilities.groovy
+++ Utilities.groovy
@@ -3,7 +3,7 @@
 
 def build_solution(location) 
          {
-            bat "\"C:/Program Files (x86)/MSBuild/14.0/bin/MSBuild.exe\" RelativityAgent1\\RelativityAgent.sln /p:Configuration=Debug /p:Platform=Any CPU"
+            bat "\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe\" RelativityAgent1\\RelativityAgent.sln"
         }
 
 return this;
