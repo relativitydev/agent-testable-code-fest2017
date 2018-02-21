0e87647b29b8dd3b1c0bf4a7719f36a470f6fd97
Priya Pande <ppande@kcura.com>
2018-02-21 15:52:31 -0600

    Added new branch to repository

--- Utilities.groovy
+++ Utilities.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 
-def build_solution(msbuildlocation) 
+def build_solution() 
          {	
             bat "\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe\" RelativityAgent1\\RelativityAgent.sln"
         }
