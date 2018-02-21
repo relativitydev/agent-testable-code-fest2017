e4df7659929469f124a3f7cd40d961cca7f1d38c
Priya Pande <ppande@kcura.com>
2018-02-21 10:19:40 -0600

    replaced release

--- RunNUnitTests.ps1
+++ RunNUnitTests.ps1
@@ -2,7 +2,7 @@ try {
     # Get all test dlls
     $nunit_dlls = Get-ChildItem -Recurse -Name -Filter *UnitTests.dll | Select-String -Pattern bin\\Debug | Sort-Object
 	
-	Write-Output -InputObject $nunit_dlls
+	echo $nunit_dlls
 
     # Run each test dll
     ForEach ($nunit_dll in $nunit_dlls) {
