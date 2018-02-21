4dfb81c4280cdb6bdb8d91f3523bd8fe73c662df
Priya Pande <ppande@kcura.com>
2018-02-21 10:13:52 -0600

    added write out

--- RunNUnitTests.ps1
+++ RunNUnitTests.ps1
@@ -1,6 +1,8 @@
 try {
     # Get all test dlls
     $nunit_dlls = Get-ChildItem -Recurse -Name -Filter *UnitTests.dll | Select-String -Pattern bin\\Release | Sort-Object
+	
+	Write-Output -InputObject $nunit_dlls
 
     # Run each test dll
     ForEach ($nunit_dll in $nunit_dlls) {
