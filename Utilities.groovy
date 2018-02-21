d023e93a9d57771ad4c50dfd1cf0926f73d6dfae
Priya Pande <ppande@kcura.com>
2018-02-21 13:18:34 -0600

    updated groovy file

--- Utilities.groovy
+++ Utilities.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
 
-def build_solution() 
+def build_solution(msbuildlocation, location) 
          {
-            bat "\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe\" RelativityAgent1\\RelativityAgent.sln"
+            bat "\"msbuildlocation" location"
         }
 		
 		
