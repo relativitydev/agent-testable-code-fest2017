3e752dadad6e14c244abbe18ca4874d2d9bb4e1d
Priya Pande <ppande@kcura.com>
2018-03-05 15:29:08 -0600

    Removed stuff

--- Utilities.groovy
+++ Utilities.groovy
@@ -8,12 +8,6 @@ def create_nunit_app_config() {
     smoke_template = smoke_template.replace('$AdminUsername', params.AdminUsername)
     smoke_template = smoke_template.replace('$RESTServerAddress', params.RESTServerAddress)
     smoke_template = smoke_template.replace('$RSAPIServerAddress', params.RSAPIServerAddress)
-    smoke_template = smoke_template.replace('$SQLServerAddress', params.SQLServerAddress)
-    smoke_template = smoke_template.replace('$SQLUsername', params.SQLUsername)
-    smoke_template = smoke_template.replace('$SQLPassword', params.SQLPassword.toString())
-    smoke_template = smoke_template.replace('$DistSQLServerAddress', params.DistSQLServerAddress)
-    smoke_template = smoke_template.replace('$DistSQLUsername', params.DistSQLUsername)
-    smoke_template = smoke_template.replace('$DistSQLPassword', params.DistSQLPassword.toString())
     
     retry(3) {
         writeFile file: "C:/${params.ProjectName}/${params.AssemblyName}.dll.config", text: smoke_template
